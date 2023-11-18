de0fcbc2652d4f35d3847a7951095e04438d6830
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-11-18 13:03:51 -0800

    init

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    var myCanvas = createCanvas(400, 400);
+    myCanvas.parent("canvasParent");
+}
+  
+  function draw() {
+    background(220);
+    ellipse(50,50,80,80);
+  }
